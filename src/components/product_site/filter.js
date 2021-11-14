defbb4a1c386cef2bf7d5c282e184a4f8de3c66c
khaitrung147 <74177325+khaitrung147@users.noreply.github.com>
2021-11-14 17:44:11 +0700

    update filter ui

--- src/components/product_site/filter.js
+++ src/components/product_site/filter.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getListCatalog } from "../../redux/actions/catalog";
+import { Slider, Checkbox } from 'antd';
+import { brandOptions, chipOptions, ramOptions, romOptions, cardOptions } from "./filterConfig";
+
 const Filter = () => {
   const catalog = useSelector((state) => state.catalogs);
 
@@ -9,37 +12,61 @@ const Filter = () => {
     dispatch(getListCatalog());
   }, [dispatch]);
 
-  return (
-    <>
-      <ul className="list-group">
-        <h5 className="fw-bolder">Thương hiệu</h5>
+  function onChangePrice(value) {
+    console.log('onChangePrice: ', value);
+  }
 
-        <li className="list-group-item brand d-flex justify-content-between align-items-center fw-bold">
-          Apple
-          <span className="badge rounded-pill">(14)</span>
+  const formatter = (value) => {
+    return  `${value*1000} vnđ`
+  }
+
+  function onChangeBrand(checkedValues) {
+    console.log('onChangeBrand = ', checkedValues);
+  }
+  
+
+  return (
+    <div className='filter-product'>
+      <ul className="list-group">
+        <h5 className="fw-bolder">Khoảng giá</h5>
+
+        <li className="list-group-item brand ">
+          <Slider
+            range
+            step={5}
+            onChangePrice={onChangePrice}
+            defaultValue={[1, 100]}
+            tipFormatter={formatter}
+          />
         </li>
       </ul>
-
       <ul className="list-group mt-3">
+        <h5 className="fw-bolder">Thương hiệu</h5>
+
+        <Checkbox.Group className='brand-checkbox' options={brandOptions} defaultValue={['Apple']} onChange={onChangeBrand} />
+      </ul>
+      <ul className="list-group cauhinh mt-3">
         <h5 className="fw-bolder">Cấu hình</h5>
 
-        <div className="form-check w-100 list-group-item config d-flex align-items-center">
-          <input
-            className="form-check"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-          <label
-            className="form-check-label w-100 fw-bold ms-2"
-            htmlFor="flexCheckDefault"
-          >
-            i5
-            <span className="badge rounded-pill float-end">(14)</span>
-          </label>
-        </div>
+        <li>
+          <h6>CPU</h6>
+          <Checkbox.Group className='brand-checkbox' options={chipOptions} defaultValue={['Core I5']} onChange={onChangeBrand} />
+        </li>
+        <li>
+          <h6>RAM</h6>
+          <Checkbox.Group className='brand-checkbox' options={ramOptions} defaultValue={['4']} onChange={onChangeBrand} />
+        </li>
+        <li>
+          <h6>Card đồ hoạ</h6>
+          <Checkbox.Group className='brand-checkbox' options={cardOptions} defaultValue={['Core I5']} onChange={onChangeBrand} />
+        </li>
+        <li>
+          <h6>Bộ nhớ trong</h6>
+          <Checkbox.Group className='brand-checkbox' options={romOptions} defaultValue={['Core I5']} onChange={onChangeBrand} />
+        </li>
+
       </ul>
-    </>
+    </div>
   );
 };
 
