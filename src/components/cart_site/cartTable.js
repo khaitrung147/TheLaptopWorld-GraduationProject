92b47c28879ab998ccec9c615b4fc2f7842eb3ba
khaitrung147 <74177325+khaitrung147@users.noreply.github.com>
2021-12-01 20:00:11 +0700

    update

--- src/components/cart_site/cartTable.js
+++ src/components/cart_site/cartTable.js
@@ -2,62 +2,46 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { Table, Tag, Space } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
-import { getListProduct } from "../../redux/actions/product";
 import './style.css'
 
-function CartTable() {
-    let data = [];
-    const dispatch = useDispatch();
-    const cart = localStorage.cart;
-    const products = useSelector((state) => state.products.data);
-    console.log('products :>> ', products);
-    if(cart){
-        data = JSON.parse(cart);
-    }
-
-    const renderProduct = (Key, CauHinh) => {
-        let product = products?.filter(e => e.Key === Key);
-        let cauhinh= product[0]?.CauHinhSanPham.filter(e => e._id === CauHinh );
-        return {
-            name: product[0]?.TenSanPham,
-            img: product[0]?.HinhAnh[0].Url,
-            cauhinh: cauhinh[0]?._id,
-            price: cauhinh[0]?.GiaSanPham
-        };
-    }
+function CartTable(props) {
+    const { renderProduct, data, loading } = props
     
-    useEffect(() => {
-        dispatch(getListProduct());
-    }, [dispatch]);
     const columns = [
         {
             title: 'Sản phẩm',
             dataIndex: 'productKey',
             key: 'productKey',
-            render: (productKey, data) => <Link to={`/san-pham/${productKey}`}>{renderProduct(productKey, data.CauHinh).name}</Link>
+            render: (productKey, data) => <Link to={`/san-pham/${productKey}`}>{renderProduct(productKey, data.CauHinh, data.SoLuong)?.name}</Link>
         },
         {
             title: 'Hình ảnh',
             dataIndex: 'productKey',
             key: 'price',
-            render: (productKey, data) => <Link to={`/san-pham/${productKey}`}><img width='100px' src={renderProduct(productKey, data.CauHinh).img} /></Link> 
+            render: (productKey, data) => <Link to={`/san-pham/${productKey}`}><img width='100px' src={renderProduct(productKey, data.CauHinh, data.SoLuong)?.img} /></Link> 
         },
         {
             title: 'Mã cấu Hình',
             dataIndex: 'productKey',
             key: 'price',
-            render: (productKey, data) => `${renderProduct(productKey, data.CauHinh).cauhinh}`
+            render: (productKey, data) => <>{renderProduct(productKey, data.CauHinh, data.SoLuong)?.cauhinh}</>
         },
         {
             title: 'Giá',
             dataIndex: 'productKey',
             key: 'price',
-            render: (productKey, data) => `${renderProduct(productKey, data.CauHinh).price}`
+            render: (productKey, data) => <>{renderProduct(productKey, data.CauHinh, data.SoLuong)?.price}</>
         },
         {
             title: 'Số lượng',
             dataIndex: 'SoLuong',
             key: 'SoLuong',
+        },
+        {
+            title: 'Thành tiền',
+            dataIndex: 'productKey',
+            key: 'totalProduct',
+            render: (productKey, data) => <>{renderProduct(productKey, data.CauHinh, data.SoLuong)?.totalProduct}</>
         },
     ];
 
@@ -66,6 +50,7 @@ function CartTable() {
         {
             data?
             <Table
+                loading={loading}
                 className='cart-table'
                 columns={columns}
                 dataSource={data}
--- src/components/cart_site/index.js
+++ src/components/cart_site/index.js
@@ -1,13 +1,92 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CartTable from './cartTable';
+import { Alert } from 'antd';
 import './style.css'
+import { CarOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from "react-redux";
+import { getListProduct } from "../../redux/actions/product";
 
 function Cart() {
+    const dispatch = useDispatch();
+    let data = [];
+    const cart = localStorage.cart;
+    const products = useSelector((state) => state.products.data);
+    const loading = useSelector(state=> state.products.load);
+    const [total, setTotal] = useState(0);
+    if(cart){
+        data = JSON.parse(cart);
+    }
+    const renderProduct = (Key, CauHinh, SoLuong) => {
+        if(data[0]){
+            let product = products?.filter(e => e.Key === Key);
+            if(product){
+                let cauhinh= product[0]?.CauHinhSanPham.filter(e => e._id === CauHinh );
+                return {
+                    name: product[0]?.TenSanPham,
+                    img: product[0]?.HinhAnh[0].Url,
+                    cauhinh: cauhinh[0]?._id,
+                    price: cauhinh[0]?.GiaSanPham,
+                    totalProduct: cauhinh[0]?.GiaSanPham * SoLuong
+                };
+            }
+            else{
+                return null;
+            }
+        }
+        else{
+            return null;
+        }
+    }
+
+    const totalRecords = () => {
+        let total= 0;
+        if(data[0]){
+            data.forEach(e => {
+                let product = products?.filter(item => item.Key === e.Key);
+                if(product){
+                    let cauhinh = product[0]?.CauHinhSanPham.filter(e => e._id === e.CauHinh );
+                    total += cauhinh?cauhinh[0].GiaSanPham * e.SoLuong:0;
+                }
+            })
+            console.log('total :>> ', total);
+            setTotal(total);
+        }
+        else{
+            setTotal(0);
+        }
+    }
+    
+    useEffect(() => {
+        totalRecords();
+        dispatch(getListProduct());
+    }, [dispatch]);
+
     return ( 
         <>
             <div className='cart-page container my-5 text-center'>
                 <h2>Giỏ hàng</h2>
-                <CartTable />
+                <div className='page-center'>
+                    <CartTable
+                        data={data}
+                        renderProduct={renderProduct}
+                        loading={loading}
+                    />
+                    <div className='row justify-content-between'>
+                        <div className='col-5 mt-3'>
+                            <Alert
+                                message="Hình thức thanh toán"
+                                description="Hiện tại shop chỉ hỗ trợ hình thức thanh toán COD (thanh toán khi nhận hàng)"
+                                type="error"
+                                icon={<CarOutlined />}
+                                showIcon
+                            />
+                        </div>
+                        <div className='col-4 mt-3'>
+                            <h5>Tổng thanh toán: <span className='text-danger'>{total}</span></h5>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </>
      );
