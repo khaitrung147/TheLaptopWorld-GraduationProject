c8dd1d824de36c361837aac75176f38e9782e79b
nhuttien123 <tienlnps12361@fpt.edu.vn>
2021-12-06 20:01:11 +0700

    Update list-feature.js

--- src/components/home_site/Feature/list-feature.js
+++ src/components/home_site/Feature/list-feature.js
@@ -17,28 +17,30 @@ function ListFeatures({ data }) {
   return (
     <>
       <Link className="col" to={"/san-pham/" + data.Key}>
-        <div className="row h-100">
-          <div className="d-flex align-items-center ">
-            <div className="col-sm-6 ">
-              <div className="w-100">
-                <img
-                  src={data.HinhAnh[0].Url}
-                  alt=""
-                  className="w-100 text-center"
-                />
+        <div className="row h-100 p-2 ">
+          <div className="card">
+            <div className="d-flex align-items-center">
+              <div className="col-sm-5">
+                <div className="w-100">
+                  <img
+                    src={data.HinhAnh[0].Url}
+                    alt=""
+                    className="w-100 text-center"
+                  />
+                </div>
               </div>
-            </div>
-            <div className="col-sm-6 ms-2">
-              <h5 className="fs-6 mt-4 fw-bolder">{data.TenSanPham}</h5>
-              <div className="mt-3">
-                <span className="">{data.CauHinhSanPham[0].HeDieuHanh}</span>
-                <p>{data.CauHinhSanPham[0].ViXuLy}</p>
-              </div>
-              <div className=" text-dark">
-                <span>{data.CauHinhSanPham[0].ManHinh}</span>
-              </div>
-              <div className="price mt-3">
-                <b className="text-info">Giá từ : {price} VNĐ </b>
+              <div className="col-sm-7 pt-4 pb-4">
+                <h5 className="fs-6 fw-bolder">{data.TenSanPham}</h5>
+                <div className="mt-3">
+                  <span className="">{data.CauHinhSanPham[0].HeDieuHanh}</span>
+                  <p>{data.CauHinhSanPham[0].ViXuLy}</p>
+                </div>
+                <div className="text-dark">
+                  <span>{data.CauHinhSanPham[0].ManHinh}</span>
+                </div>
+                <div className="price mt-3">
+                  <b className="text-info">Giá từ : {price} VNĐ </b>
+                </div>
               </div>
             </div>
           </div>
